24d9aa220353ad35ad86089ea3614136735ba6e4
parveensuyan <parveensuyan@gmail.com>
2022-03-09 17:11:59 -0800

    READ AND WRITE FILE OPERATION (TEXT AND CSV FILE)

---
+++ readAwrite.php
@@ -0,0 +1,37 @@
+<?php
+// $file = fopen("age.csv","r");
+// while(! feof($file))
+//   {
+//   print_r(fgetcsv($file));
+//   }
+// fclose($file);
+
+$filename = "newfile.txt";
+$file = fopen( $filename, "w" );
+if( $file == false ) {
+echo ( "Error in opening new file" );  exit();
+}
+fwrite( $file, "This is a simple test\n" );
+fclose( $file );
+?>
+<html>
+<head>
+<title>Writing a file using PHP</title>
+</head>
+<body>
+<?php
+$filename = "newfile.txt";
+$file = fopen( $filename, "r" );
+if( $file == false ) {
+echo ( "Error in opening file" );
+exit();
+}
+$filesize = filesize( $filename );
+$filetext = fread( $file, $filesize );
+fclose( $file );
+echo ( "File size : $filesize bytes" );
+echo ( "$filetext" );
+echo("file name: $filename");
+?>
+</body>
+</html>
