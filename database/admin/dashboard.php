32b1534dec23d6133a09816b551448a75693154f
parveensuyan <parveensuyan@gmail.com>
2022-03-16 15:51:39 -0700

    new changes

--- database/admin/dashboard.php
+++ database/admin/dashboard.php
@@ -1,10 +1,8 @@
 <?php
  session_start();
 $user_id =  $_SESSION['user_id'];
-// unset($_SESSION);
 $array_result = InsertValue($user_id);
-// print_r($array_result);
-// exit;
+
     function InsertValue($user_id){
             $array_result = array();
 
@@ -20,13 +18,15 @@ $array_result = InsertValue($user_id);
             $sql = "select * from contactus";    
            
             $result = $conn->query($sql);
-            if($result->num_rows >0){
+            if($result->num_rows > 0){
                 // $_POST['username'] = 
                 $array_result = $result->fetch_all(MYSQLI_ASSOC); 
-
+            // include 'include_table.php';
+              // print_r($test);
              }
             else{
-                echo "error".$conn->connect_error;
+              // echo 1;
+                echo $conn->connect_error;
                 }
                 $conn->close();
                 return $array_result;
@@ -39,34 +39,20 @@ $array_result = InsertValue($user_id);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messages</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="admin.css">
 </head>
 <body>
     <p>HI ADMIN</p>
 <table id="customers">
-  <tr>
-    <th>Name</th>
-    <th>Email</th>
-    <th>Mobile</th>
-    <th>Message</th>
-    <th>Action</th>
-  </tr>
- <?php 
-// print_r($array_result);
- 
- foreach($array_result as $value){?>
-  <tr>
-    <td><?php echo $value['firstname'];?></td>
-    <td><?php echo $value['email'];?></td>
-    <td><?php echo $value['mobile'];?></td>
-    <td><?php echo $value['message'];?></td>
-    <td><a href="edit.php/<?php echo $value['id'];?>">Edit</a> | <a href="delete.php/<?php echo $value['id'];?>">Delete</a></td>
-  </tr>
- <?php } ?>
+ <?php include 'include_table.php' ; ?>
  
 </table>
-
 </body>
 </html>
+<!-- dashboard where view all of the record
+index_admin.js jquery
+delete.php delte the entity and generate a new html
 
+-->
 
